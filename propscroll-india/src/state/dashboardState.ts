25f4825e360aaf96b4126613355adfdebfd3f4b8
divyanibhusari <divyanibhusari79@gmail.com>
2026-07-30 16:08:23 +0530

    update

--- propscroll-india/src/state/dashboardState.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from "jotai";
-import { DashboardStats } from "@/src/api/dashboardApi";
-
-export const dashboardStatsAtom = atom<DashboardStats | null>(null);
-export const dashboardLoadingAtom = atom<boolean>(false);
-export const dashboardErrorAtom = atom<string>("");
--- propscroll-india/src/state/notificationState.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from "jotai";
-import { Notification } from "@/src/api/notificationApi";
-
-export const notificationsAtom = atom<Notification[]>([]);
-export const notifLoadingAtom = atom<boolean>(false);
-export const unreadCountAtom = atom<number>(0);
